0eddd42edf5ba52aef4c4ad4bf9edfd369d77155
Gouravshandilya69 <i.gouravshandilya@gmail.com>
2023-11-06 19:28:44 +0530

    safag

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const fileUpload=require("express-fileupload")
 dbconnect()
 
 const corsOptions = {
-    origin: 'http://jobx-frontend.vercel.app/',
+    origin: 'https://jobx-frontend.vercel.app/',
     credentials: true,  
   };
 //   http://localhost:5173
